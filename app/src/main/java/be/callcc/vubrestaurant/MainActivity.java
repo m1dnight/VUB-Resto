fb887a47a51f9228b9b269718d0f1d3925717deb
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-09-08 14:04:37 +0200

    upgraded to android x

--- app/src/main/java/be/callcc/vubrestaurant/MainActivity.java
+++ app/src/main/java/be/callcc/vubrestaurant/MainActivity.java
@@ -3,15 +3,17 @@ package be.callcc.vubrestaurant;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.PagerTabStrip;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.PagerTabStrip;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.ActionBar;
+//import androidx.appcompat.app.ActionBarActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
@@ -26,7 +28,7 @@ import be.callcc.vubrestaurant.core.Constants;
 import be.callcc.vubrestaurant.core.RestaurantContainer;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     @SuppressWarnings("unused")
     private static final String TAG = "MainActivity";
 
